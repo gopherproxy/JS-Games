ab500c80622d2388200a5333cb8d6a0fa95e5bea
gopherproxy <gopherproxy@gmail.com>
2018-03-14 10:30:20 +0100

    1. mandatory assignment ("Hot dice"): Solution

--- 04_hot_dice.js
+++ 04_hot_dice.js
@@ -1,31 +1,55 @@
+// JavaScript Document
+
 class Dice {
 
 	constructor(name) {
-		this.name = name; // constructor takes an attribute 'name' in order to pass on a unique dice-ID
+		this.name = name;
 		this.face = 0;
 	}
 
 	rollDice() {
 		this.face = Math.ceil((Math.random()) * 6); // returning whole number between 1 - 6
 		console.log(this.name + ': ' + this.face);
-		// printing the result to an HTML element with the same ID
+		// printing the result
 		document.getElementById(this.name).innerHTML = this.face;
 	}
-	
-	get result() {
-		return this.face; // getter to read the result of one dice
-		}
 
+	get result() {
+			return this.face;
+		}
 		// END class Dice
 }
 
 class Player {
 
 	constructor() {
-		this.diceResult = 0; // player properties 
+		this.name = '';
+		this.diceResult = 0;
+		this.score = 0;
 	}
 
-	rollDices(id1, id2, output) { // call to the dice class - 2 dice instantiated and rolled
+	playerName() {
+		// prepare client storage for user + score if doesn't exist
+		if (!localStorage.getItem('user')) {
+			localStorage.setItem('user', '');
+			localStorage.setItem('userScore', 0);
+		}
+		// prompt for user name if not yet set
+		if (localStorage.getItem('user') === '') {
+			this.name = prompt("Please enter your name", "Mr. X");
+			// set username
+			localStorage.setItem('user', this.name);
+		}
+		// get player name from local storage
+		var playername = localStorage.getItem('user');
+		this.name = playername;
+		this.score = localStorage.getItem('userScore');
+		alert('Hi ' + this.name + ', your all-time score is ' + this.score + '.  Ready to roll?');
+		document.getElementById("pName").innerHTML = this.name;
+		document.getElementById("pScore").innerHTML = this.score;
+	}
+
+	rollDices(id1, id2, output) {
 		let dice1 = new Dice(id1);
 		dice1.rollDice();
 
@@ -37,7 +61,7 @@ class Player {
 	}
 
 	get result() {
-			return this.diceResult; // getter to read the result of the two dice
+			return this.diceResult;
 		}
 		// END class Player
 }
@@ -49,8 +73,47 @@ class Player {
 function gameLoop() {
 	'use strict';
 
+	document.getElementById('playAgain').addEventListener('click', playAgain);
+	document.getElementById('clearData').addEventListener('click', clearData);
+
 	var player1 = new Player();
+	player1.playerName();
 	player1.rollDices('dice1', 'dice2', 'pResult');
+
+	var computer = new Player();
+	computer.rollDices('dice3', 'dice4', 'cResult');
+
+	compare();
+
+	function compare() {
+		if (player1.result < computer.result) {
+			computer.score++;
+			document.getElementById("winLoose").innerHTML = 'You loose!';
+			document.getElementById("cScore").innerHTML = computer.score;
+		} else if (player1.result > computer.result) {
+			player1.score++;
+			document.getElementById("winLoose").innerHTML = 'Nice one!';
+			document.getElementById("pScore").innerHTML = player1.score;
+			localStorage.setItem('userScore', player1.score);
+			console.log('Score: ' + player1.score);
+		} else {
+			document.getElementById("winLoose").innerHTML = 'Draw...';
+		}
+	}
+
+	function playAgain() {
+		player1.rollDices('dice1', 'dice2', 'pResult');
+		computer.rollDices('dice3', 'dice4', 'cResult');
+		compare();
+	}
+
+	function clearData() {
+		alert('Do you really want to clear ALL data?');
+		localStorage.clear();
+		location.reload();
+		// Clear all keys
+		// store.clearAll();
+	}
 
 }
 
